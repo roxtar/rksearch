81ad0f3b52e4b8cde1744784f853b15ab1c8bd8c
Rohit Kumar <roxtar@gmail.com>
2010-11-12 12:58:19 -0800

    Switched to a better name for the SequenceGenerator project

--- RabinKarpSearchTest/rksimplecomparetest.cs
+++ RabinKarpSearchTest/rksimplecomparetest.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RabinKarpSearch;
-using SequenceGenerator;
+using Utilities;
 namespace RabinKarpSearchTest
 {
     [TestClass]
